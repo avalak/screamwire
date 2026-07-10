1e568e07263b6d931db9efab35af938693e40915
Avalak <avalak@users.noreply.github.com>
2026-07-10 15:41:15 +0400

    fix(sender): make event bridge RT-safe with poll-based blocking wait

--- sender/src/event_bridge.rs
+++ sender/src/event_bridge.rs
@@ -1,8 +1,11 @@
 //! Lightweight, lock‑free bridge for waking up the network sender when
 //! new audio data is available.
 //!
-//! Based on Linux's [`eventfd`]
+//! Based on Linux's [`eventfd`](https://man7.org/linux/man-pages/man2/eventfd.2.html).
+use nix::libc;
 use nix::sys::eventfd::{EfdFlags, EventFd};
+use std::io;
+use std::os::fd::AsRawFd;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -14,8 +17,8 @@ pub struct StreamEventBridge {
 
 impl StreamEventBridge {
     pub fn new() -> Self {
-        let event_fd = EventFd::from_value_and_flags(0, EfdFlags::EFD_CLOEXEC)
-            .expect("Failed to create eventfd");
+        let flags = EfdFlags::EFD_CLOEXEC | EfdFlags::EFD_NONBLOCK;
+        let event_fd = EventFd::from_value_and_flags(0, flags).expect("Failed to create eventfd");
 
         Self {
             inner: Arc::new(event_fd),
@@ -23,23 +26,55 @@ impl StreamEventBridge {
         }
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn notify_data_ready(&self) {
         let _ = self.inner.write(1);
     }
 
     pub fn wait_for_data(&self) {
-        // TODO: gracefull shutdown?
-        let _ = self.inner.read();
+        let fd = self.inner.as_raw_fd();
+
+        let mut fds = [libc::pollfd {
+            fd,
+            events: libc::POLLIN,
+            revents: 0,
+        }];
+
+        let ret = unsafe { libc::poll(fds.as_mut_ptr(), 1, -1) };
+
+        match ret {
+            // Success
+            n if n > 0 && (fds[0].revents & libc::POLLIN) != 0 => {
+                let mut value: u64 = 0;
+                let _ = unsafe {
+                    libc::read(
+                        fd,
+                        &mut value as *mut u64 as *mut libc::c_void,
+                        std::mem::size_of::<u64>(),
+                    )
+                };
+            }
+
+            // Signal interrupted
+            n if n < 0 => {
+                let err = io::Error::last_os_error();
+                if err.kind() != io::ErrorKind::Interrupted {
+                    log::error!("StreamEventBridge: poll failed: {}", err);
+                }
+            }
+
+            // No action
+            _ => {}
+        }
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn notify_flush(&self) {
         self.flush_requested.store(true, Ordering::Release);
         let _ = self.inner.write(1);
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn swap_flush_requested(&self) -> bool {
         self.flush_requested.swap(false, Ordering::Acquire)
     }
